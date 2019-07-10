785bf3f8e74852f821b8553161cbc5f2706da49e
Boopathi <Boopathi@LAPTOP-586KSOHN>
2019-07-10 10:07:22 +0530

    changes

--- spring-boot-registration-application-front/src/main/java/com/spring/boot/registration/application/SpringBootRegistrationApplicationFrontApplication.java
+++ spring-boot-registration-application-front/src/main/java/com/spring/boot/registration/application/SpringBootRegistrationApplicationFrontApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootRegistrationApplicationFrontApplication {
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringBootRegistrationApplicationFrontApplication.class, args);
 	}
 
