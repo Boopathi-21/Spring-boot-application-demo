fed64d67a8db61bea8875b2b6ac2b87d11483bf2
Boopathi <Boopathi@LAPTOP-586KSOHN>
2019-09-22 18:46:46 +0530

    alignment fix

--- bank-management-scb/src/main/java/com/scb/bank/management/BankManagementScbApplication.java
+++ bank-management-scb/src/main/java/com/scb/bank/management/BankManagementScbApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankManagementScbApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BankManagementScbApplication.class, args);
 	}
